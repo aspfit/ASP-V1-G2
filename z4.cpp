4ea724c36647539ac65a60b64f7ffad8129e4d38
Alem Tatarević [Redovan] <IB160042@fit.ba>
2017-10-13 11:58:29 +0200

    Added few new function

---
+++ z4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "linkedList.h"
+
+using namespace std;
+
+int main() {
+	LinkedList<int> list;
+	list.addFirst(3);
+	list.addFirst(4);
+	list.addFirst(5);
+	list.addLast(1); 
+	list.deleteFirst();
+	list.deleteLast();
+	list.displayList();
+
+	return 0;
+}
